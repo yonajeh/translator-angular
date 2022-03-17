e6e8c5039c939e0c91e61b9c0bb522e27a3cce71
Youssef NAJEH <yo.najeh@gmail.com>
2022-03-18 00:47:56 +0100

    project is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'translator-angular';
+  text: string = '';
+  textTraduit: string = '';
+  langFrom: string = '';
+  langTo: string = '';
+
+  constructor(private http: HttpClient) {
+  }
+
+  traduire() {
+    // text a traduire + langFrom + langTo
+    // call au backend
+    const url = "https://just-translated.p.rapidapi.com/?lang=" + this.langFrom + "-" + this.langTo + "&text=" + this.text
+    console.log('URL : ', url);
+
+    const headers = new HttpHeaders()
+      .set('X-Rapidapi-Key', '3628e260bcmshca4d6a45f5e4aa2p1c994bjsne41d8ea97ec0');
+
+    this.http.get(url, {headers: headers}).subscribe(
+      data => this.showResult(data),
+      error => console.log(error),
+      () => console.log("done")
+    )
+
+  }
+
+
+  showResult(data: any) {
+    this.textTraduit = data.text;
+  }
 }
